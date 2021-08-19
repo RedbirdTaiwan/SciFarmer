72a270b57c5026c024ba268a96ea1bef127f439a
Redbird(Shihhung) Wu <redbird.wu@gmail.com>
2021-08-19 17:25:18 +0800

    update index.html farmland.html

---
+++ sci-farmer.js
@@ -0,0 +1,9 @@
+const farms = {
+    iNat: {
+        農田裏的科學計畫: {
+            project_id: 'cb218e40-a6ce-4547-b29b-b84619353ae4',
+            location: '宜蘭員山',
+            type: '水稻田',
+        }
+    }
+}
