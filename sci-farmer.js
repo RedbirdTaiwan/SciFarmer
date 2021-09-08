5448d961701f6a49eaa6ab7ea6acde6cacb11568
Redbird(Shihhung) Wu <redbird.wu@gmail.com>
2021-09-08 13:58:22 +0800

    Update sci-farmer.js

--- sci-farmer.js
+++ sci-farmer.js
@@ -1,31 +1,31 @@
 const farmlands = {
-    farmland01: {
-        name: '農田裏的科學計畫',
-        farmers: ['farmer01'],
-        iNat: {
-            project_id: 'cb218e40-a6ce-4547-b29b-b84619353ae4',
-            location: '宜蘭員山',
-            type: '水稻田',
+    "farmland01": {
+        "name": "農田裏的科學計畫",
+        "farmers": ["farmer01"],
+        "iNat": {
+            "project_id": "cb218e40-a6ce-4547-b29b-b84619353ae4",
+            "location": "宜蘭員山",
+            "type": "水稻田",
         },
-        eBird: {
-            hotspot_id: 'L15510987'
+        "eBird": {
+            "hotspot_id": "L15510987"
         },
-        location: '宜蘭員山',
-        type: '水稻田',
-        website: 'https://www.scienceinfield.com/',
+        "location": "宜蘭員山",
+        "type": "水稻田",
+        "website": "https://www.scienceinfield.com/"
     }
 }
 
 const farmers = {
-    farmer01: {
-        name: '林芳儀',
-        farmlands: ['farmland01'],
-        fb: 'https://www.facebook.com/fangyee.lin',
-        iNat: {
-            user_id: '737771',
+    "farmer01": {
+        "name": "林芳儀",
+        "farmlands": ["farmland01"],
+        "fb": "https://www.facebook.com/fangyee.lin",
+        "iNat": {
+            "user_id": "737771",
         },
-        eBird: {
-            user_id: '',
+        "eBird": {
+            "user_id": "",
         },
     }
 }
