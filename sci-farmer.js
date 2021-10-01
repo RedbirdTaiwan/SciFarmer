4b2b1f41af3766c0a6c8333eb46c0e48c0431283
Redbird(Shihhung) Wu <redbird.wu@gmail.com>
2021-10-01 12:01:41 +0800

    add product paras.

--- sci-farmer.js
+++ sci-farmer.js
@@ -2,53 +2,41 @@ const farmlands = {
     "farmland01": {
         "name": "農田裏的科學計畫",
         "farmers": ["farmer01"],
-        "iNat": {
-            "project_id": "cb218e40-a6ce-4547-b29b-b84619353ae4"
-        },
-        "eBird": {
-            "hotspot_id": "L15510987"
-        },
+        "iNat": "cb218e40-a6ce-4547-b29b-b84619353ae4",
+        "eBird": "L15510987",
         "location": "宜蘭員山",
         "type": "水稻田",
+        "product": "高雄147白米、高雄147糙米、蜜釀柚子醋、無調味米餅、蓮藕",
         "website": "https://www.scienceinfield.com/"
     },
     "farmland02": {
         "name": "美濃龍肚友善田",
         "farmers": ["farmer02"],
-        "iNat": {
-            "project_id": "b5c008d3-d251-401d-880b-38fa10aad05b"
-        },
-        "eBird": {
-            "hotspot_id": ""
-        },
+        "iNat": "b5c008d3-d251-401d-880b-38fa10aad05b",
+        "eBird": "",
         "location": "高雄美濃",
         "type": "水稻田",
+        "product": "",
         "website": "#"
     },
     "farmland03": {
         "name": "美濃湖水雉棲地",
         "farmers": ["farmer02", "farmer03"],
-        "iNat": {
-            "project_id": "ab3fc249-2594-4872-9b01-0e5ea75bf4a1"
-        },
-        "eBird": {
-            "hotspot_id": "L7813489"
-        },
+        "iNat": "ab3fc249-2594-4872-9b01-0e5ea75bf4a1",
+        "eBird": "L7813489",
         "location": "高雄美濃",
         "type": "菱角",
+        "product": "",
         "website": "https://www.facebook.com/groups/1977255549258794/"
     },
     "farmland04": {
         "name": "田鱉米農場",
         "farmers": ["farmer04"],
-        "iNat": {
-            "project_id": "6b741243-98bb-43a7-9b78-407445618fb9"
-        },
-        "eBird": {
-            "hotspot_id": "L5437766"
-        },
+        "iNat": "6b741243-98bb-43a7-9b78-407445618fb9",
+        "eBird": "L5437766",
         "location": "苗栗通霄",
         "type": "水稻田",
+        "product": "台中192田鱉米、高雄選10號黃豆",
         "website": "https://giantwaterbug.blogspot.com/"
     }
 }
@@ -58,44 +46,28 @@ const farmers = {
         "name": "林芳儀",
         "farmlands": ["farmland01"],
         "fb": "https://www.facebook.com/fangyee.lin",
-        "iNat": {
-            "user_id": "linfy66"
-        },
-        "eBird": {
-            "user_id": ""
-        }
+        "iNat": "linfy66",
+        "eBird": ""
     },
     "farmer02": {
         "name": "陳柏豪",
         "farmlands": ["farmland02", "farmland03"],
         "fb": "https://www.facebook.com/604738989/",
-        "iNat": {
-            "user_id": "pohaochen"
-        },
-        "eBird": {
-            "user_id": ""
-        }
+        "iNat": "pohaochen",
+        "eBird": ""
     },
     "farmer03": {
         "name": "高雄鳥會",
         "farmlands": ["farmland03"],
         "fb": "https://www.facebook.com/kwbs1979/",
-        "iNat": {
-            "user_id": ""
-        },
-        "eBird": {
-            "user_id": "高雄 鳥會"
-        }
+        "iNat": "",
+        "eBird": "高雄 鳥會"
     },
     "farmer04": {
         "name": "王正安",
         "farmlands": ["farmland04"],
         "fb": "https://www.facebook.com/q24518948",
-        "iNat": {
-            "user_id": "anglerfish890"
-        },
-        "eBird": {
-            "user_id": "ZhengAn Wang"
-        }
+        "iNat": "anglerfish890",
+        "eBird": "ZhengAn Wang"
     }
 }
